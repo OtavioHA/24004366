e0dab93e9b70d12353476465191e50c26cce1279
Otavio <113810044+OtavioHA@users.noreply.github.com>
2024-08-16 12:47:04 -0300

    Update and rename exercicio01 to exercicio001.cpp

---
+++ exercicios/exercicio001.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int num;
+
+int main(){
+    printf("Digite a quantidade de visitantes do dia de hoje:");
+    scanf("%i",&num);
+    printf("A quantidade de visitantes foi de: %i pessoas",num);
+    
+}
